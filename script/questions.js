62a6b86a8dd271486e5986a8b67df8f5c8cf2cc5
uchrissd <uchrissd@protonmail.com>
2019-10-09 17:38:49 -0700

    added questions to the question object in the questions script and created the main js script

--- script/questions.js
+++ script/questions.js
@@ -8,5 +8,27 @@ var questions = [
     title: "The condition in an if / else statement is enclosed within ____.",
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
     answer: "parentheses"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store:",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ],
+    answer: "all of the above"
+  },
+  {
+    title:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parenthese"],
+    answer: "quotes"
+  },
+  {
+    title:
+      "A very useful tool used during development ad debuggins for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    answer: "console.log"
   }
 ];
