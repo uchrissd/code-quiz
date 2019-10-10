8744bd9182f705b7569b602356421c6cebde4698
uchrissd <uchrissd@protonmail.com>
2019-10-10 11:10:25 -0700

    updated the html file, script file

---
+++ script/script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  console.log("starting");
+};
+
+//Variables for index, countdown, score
+
+// var index = 0;
+// var countDown = 15;
+// var score = 0;
+
+//Iniate the quiz on click of the start button
+
+document.getElementById("start-button").addEventListener("click", event => {
+  console.log("hello");
+});
