74b2c45ebf291a4488587c6fcb2b0109701c1831
uchrissd <uchrissd@protonmail.com>
2019-10-11 10:52:16 -0700

    made updates to the html file, added additional functions to the script file, modified the objects in the questions array in the questions script

--- script/questions.js
+++ script/questions.js
@@ -11,24 +11,26 @@ var questions = [
   },
   {
     title: "Arrays in JavaScript can be used to store:",
-    choices: [
-      "numbers and strings",
-      "other arrays",
-      "booleans",
-      "all of the above"
-    ],
+    choices: [{text: "numbers and strings", answer: false},{text: "other arrays", answer: false},{text: "booleans", answer: false},{text: "all of the above", answer: true}],
     answer: "all of the above"
   },
   {
     title:
       "String values must be enclosed within ____ when being assigned to variables.",
-    choices: ["commas", "curly brackets", "quotes", "parenthese"],
+    choices: [{text:"commas", answer: false}, {text: "curly brackets", answer:false}, {text: "quotes", answer: true}, {text:"parenthese", answer: false}],
     answer: "quotes"
   },
   {
     title:
       "A very useful tool used during development add debugging for printing content to the debugger is:",
-    choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    choices: [{text: "JavaScript", answer: false}, {text: "terminal/bash", answer: false}, {text: "for loops", asnwer: false}, {text: "console.log", answer: true}],
     answer: "console.log"
   }
 ];
+
+
+
+{
+  title: "Commonly used data types DO NOT include:",
+  choices: [{text: 'strings', answer: false}, {text: 'alerts', answer: true}],
+},
--- script/script.js
+++ script/script.js
@@ -4,27 +4,46 @@ window.onload = function() {
 
 //Variables for index, countdown, score
 
-// import exportQuestions  from "questions.js";
-// var questions = exportQuestions.exportQuestions();
-// console.log(questions);
-
 var index = 0;
 var countDown = 15;
 var score = 0;
+
+//DOM variables
+
+//Button text
 
 //Iniate the quiz on click of the start button
 
 document.getElementById("start-button").addEventListener("click", event => {
   console.log("hello");
+  document.getElementById("start-quiz").classList.add("d-none");
+  document.getElementById("quiz-questions").classList.remove("d-none");
+  renderQuestions();
 });
 
-//Write the for loop to loop through the array
+//Loop through array to grab questions from object and present them to screen with buttons for the multiple choices
 
-function questionLoop() {
-  for (var i = 0; i < questions.length; i++) {
-    console.log(i);
+function renderQuestions() {
+  var questionsIndexLength = questions.length - 1;
+  if (index <= questionsIndexLength) {
+    document.getElementById("question").innerHTML = questions[index].title;
+    renderQuestionChoices();
   }
 }
+
+function renderQuestionChoices() {
+  var questionChoicesArray = questions[index].choices;
+  for (var i = 0; i < questionChoicesArray.length; i++) {
+    console.log(i);
+    var questionChoicesEl = document.getElementById("question-choices");
+    var options = document.createElement("button");
+    options.innerHTML = questionChoicesArray[i].text;
+    options.setAttribute("value", questionChoicesArray[i].answer);
+    questionChoicesEl.append(options);
+  }
+}
+
+function goToNextQuestion() {}
 
 //I need a function that creates buttons
 //I need a function that captures the events
