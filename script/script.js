7a5ba92800fc3fcbc979313b77b10aedf9a3f59c
uchrissd <uchrissd@protonmail.com>
2019-10-11 21:50:12 -0700

    changed the name of an id in the html file, changed the names of some variables in the sript file and created a time function

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@ window.onload = function() {
 //Global ariables for index, countdown, score
 
 var index = 0;
-var countDown = 15;
+var countDown = 75;
 var score = 0;
 var highScore = 0;
 
@@ -57,13 +57,33 @@ function checkAnswer(question, answer) {
   let userAnswer = questions[question].choices[answer];
   if (userAnswer == correctAnswer) {
     index = index + 1;
+    score = score + 1;
+    console.log(score);
     console.log("Next question: ", index);
     clearQuestionDiv();
     renderQuestions();
 
     console.log("Correct");
-  } else console.log("Incorrect");
+  } else alert("Uh oh! That's the wrong answer. Try again!");
+
+  console.log("Incorrect");
 }
+
+//This variable sets the time for the quiz by taking in the set time function
+var quizTime = setInterval(setTime, 1000);
+
+//This function starts the countdown for the time left clock quiz timer
+
+function setTime() {
+  document.getElementById("quiz-time").innerHTML = countDown + "sec left";
+  countDown--;
+  if (countDown == -1) {
+    clearInterval(quizTime);
+    alert("Time out!!");
+  }
+}
+
+function userScore() {}
 
 //I need a function that creates buttons
 //I need a function that captures the events
